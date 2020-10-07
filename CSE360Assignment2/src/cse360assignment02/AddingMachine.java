8c42678de9a9811d27cd3677ce14b7d03f7f6f84
mbuya <mbuya@DESKTOP-LD92F2D>
2020-10-07 12:00:36 -0700

    Adjusted to include comments regarding what the program would have to do.

--- CSE360Assignment2/src/cse360assignment02/AddingMachine.java
+++ CSE360Assignment2/src/cse360assignment02/AddingMachine.java
@@ -4,34 +4,45 @@ package cse360assignment02;
  * <h1> This program is tasked with adding and subtracting numbers</h1>
  * 
  * <p>
+ * <b>Note</b>: The code has been adjusted slightly because I like it to look this way rather than the way it was provided.
  * 
  * @author 		Michael Buyanin
- * @version 	1.0
+ * @version 		1.0
  * @since		October 7th, 2020
  */
 
 
-public class AddingMachine {
+public class AddingMachine 
+{
   private int total;
   
-  public AddingMachine () {
+  public AddingMachine () 
+  {
     total = 0;  // not needed - included for clarity
   }
   
-  public int getTotal () {
-    return 0;
+  public int getTotal () 
+  {
+    return 0; //This will have to be adjusted to get the private int total.
   }
   
-  public void add (int value) {
+  public void add (int value) 
+  {
+	  //This will have to add value to the total.
   }
 
-  public void subtract (int value) {
+  public void subtract (int value) 
+  {
+	  //This will have to subtract value from the total.
   }
 
-  public String toString () {
-    return "";
+  public String toString () 
+  {
+    return ""; //Converts total to a string
   }
 
-  public void clear() {
+  public void clear() 
+  {
+	  //Clears private int total.
   }
 }
