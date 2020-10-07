4d22a96f26cbbf02f57b4bd9ff8ef0e95e8c55cb
mbuya <mbuya@DESKTOP-LD92F2D>
2020-10-07 11:50:32 -0700

    Creation of project "AddingMachine". This is for CSE 360.

---
+++ CSE360Assignment2/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,37 @@
+package cse360assignment02;
+
+/*
+ * <h1> This program is tasked with adding and subtracting numbers</h1>
+ * 
+ * <p>
+ * 
+ * @author 		Michael Buyanin
+ * @version 	1.0
+ * @since		October 7th, 2020
+ */
+
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
+
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
